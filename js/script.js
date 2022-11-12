30b5550291e10a720b58f6843568a7e7117575d7
tisagar11 <tariqulislamsagar.cse@gmail.com>
2022-11-12 15:57:03 +0600

    js example 1 done

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// ==== Javascript ====
+
+function textChange(){
+	document.getElementById('js_p').innerHTML="Congratulations. You have made it.";
+};
+
+
+
+
+// ==== jQuery ====
+
+$(document).ready(function(){
+	$(selector).event(effect);
+});
